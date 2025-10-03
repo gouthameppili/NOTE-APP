5e599082a2cfa21f3d751c415f6a5852fb2382f5
gouthameppili <eppiligoutham@gmail.com>
2025-10-03 17:47:05 +0530

    Fix Navbar import path in HomePage.jsx

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import NavBar from '../components/Navbar';
+import NavBar from '../components/NavBar';
 import RateLimitedUI from '../components/RateLimitedUI';
 // import axios from 'axios';
 import api from "../lib/axios";
